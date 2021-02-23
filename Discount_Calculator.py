1a87a352d451db94de9d2ae8bb53dea78ddde446
RedPlumDragon <79021820+RedPlumDragon@users.noreply.github.com>
2021-02-23 12:40:35 -0600

    calculate how much to pay with discount

---
+++ Discount_Calculator.py
@@ -0,0 +1,17 @@
+# imports module
+from decimal import Decimal
+
+# payment
+payment = input("What is the original price? → ")
+
+# discount percentage
+discount = input("What is the percent discount? → ")
+
+# amount of discount
+amount = (Decimal(discount) / 100) * Decimal(payment)
+
+# what you have to pay
+after_discount = Decimal(payment) - Decimal(amount)
+
+# result
+print("The amount you have to pay is", after_discount)
