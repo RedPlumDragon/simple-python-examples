b0e0b8236740d52c1bba77d82f1849cb8a9d63c6
RedPlumDragon <79021820+RedPlumDragon@users.noreply.github.com>
2021-02-24 09:49:31 -0600

    Have a simple conversation with Python

--- Conversation.py
+++ Conversation.py
@@ -5,49 +5,45 @@ from getpass import getuser
 
 # user puts in their name
 name = getuser()
-name_check = input("Is your name " + name + "? → ")
-if name_check.lower().startswith("y"):
-    print("Okay.")
+name_check = input('Is your name ' + name + '? → ')
+if name_check.lower().startswith('y'):
+    print('Okay.')
     time.sleep(1)
 
-if name_check.lower().startswith("n"):
-    name = input("Then what is it? → ")
+if name_check.lower().startswith('n'):
+    name = input('Then what is it? → ')
 
 # Python lists their name
 userList = name
 
 # Python & user dialoge
-print("Hello", name + ", my name is Python." )
+print('Hello', name + ', my name is Python.' )
 time.sleep(0.8)
-print("The first letter of your name is", userList[0] + ".")
+print('The first letter of your name is', userList[0] + '.')
 time.sleep(0.8)
-print("Nice to meet you. :)")
+print('Nice to meet you. :)')
 time.sleep(0.8)
 response = input("Would you say it's nice to meet me? → ")
 
 # other dialoge
-if response.lower().startswith("y"):
-    print("Nice :)")
+if response.lower().startswith('y'):
+    print('Nice :)')
     sys.exit()
 
-elif response.lower().startswith("n"):
-    response2 = input("Is it because I am a robot? → ")
+elif response.lower().startswith('n'):
+    response2 = input('Is it because I am a robot? → ')
 
 else:
-    print("You may have made an input error. Please restart and try again.")
+    print('You may have made an input error. Please restart and try again.')
     sys.exit()
-if response2.lower().startswith("y"):
-    print("Aw :(")
+if response2.lower().startswith('y'):
+    print('Aw :(')
 
-elif response2.lower().startswith("n"):
-    response3 = input("Then why? → ")
+elif response2.lower().startswith('n'):
+    response3 = input('Then why? → ')
     time.sleep(1)
-    print("Oh.")
+    print('Oh.')
 
 else:
-    print("You may have made an input error. Please restart and try again.")
+    print('You may have made an input error. Please restart and try again.')
     sys.exit()
-    
-
-
-
