7d0e78678c443120091a10d2ca89542f7c155072
RedPlumDragon <79021820+RedPlumDragon@users.noreply.github.com>
2021-02-24 09:52:30 -0600

    prints the multiplication table for a number

--- Multiplication_Table.py
+++ Multiplication_Table.py
@@ -1,19 +1,19 @@
 import sys
 
 try:
-    number = int(input("Enter the number you want multiplied. → "))
+    number = int(input('Enter the number you want multiplied. → '))
 
 except:
-    print("You did not enter a number.")
+    print('You did not enter a number.')
     sys.exit()
 
 
 try:
-    for multiplication in range(int(input("How many times do you want your number multiplied? → "))):
+    for multiplication in range(int(input('How many times do you want your number multiplied? → '))):
         multiplication = multiplication + 1
         product = number * multiplication
-        print(number, "x", multiplication, "=", product)
+        print(number, 'x', multiplication, '=', product)
 
 except:
-    print("You did not enter a number.")
+    print('You did not enter a number.')
     sys.exit()
